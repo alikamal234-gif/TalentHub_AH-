6e615a8b99b4983997949912339b01166cc14902
Hamza Hajjaji <hh.hajjaji.hamza@gmail.com>
2026-01-19 22:36:02 +0100

    refactor(repository): mettre à jour les dépendances et améliorer la structure
    - Remplacement de \PDO par PDO dans les annotations de type
    - Suppression du paramètre $tableName du constructeur
    - Ajout d'une méthode abstraite getTableName() pour obtenir le nom de la table
    - Mise à jour de l'initialisation de tableName via la nouvelle méthode abstraite
    - Réorganisation du code avec des séparations claires entre méthodes

--- core/Repository/AbstractRepository.php
+++ core/Repository/AbstractRepository.php
@@ -4,21 +4,24 @@ namespace Core\Repository;
 
 
 use Core\Database\Connection;
+use PDO;
 
 abstract class AbstractRepository
 {
-    protected \PDO $pdo;
+    protected PDO $pdo;
     protected string $tableName;
-    public function __construct(Connection $connection, string $tableName)
+
+    public function __construct(Connection $connection)
     {
         $this->pdo = $connection->getConnection();
-        $this->tableName = $tableName;
+        $this->tableName = $this->getTableName();
     }
 
     protected function findAll(): array
     {
         return $this->pdo->query("SELECT * FROM {$this->tableName}")->fetchAll();
     }
+
     protected function find(int $id): ?array
     {
         $stmt = $this->pdo->prepare("SELECT * FROM {$this->tableName} WHERE id = :id");
@@ -27,6 +30,8 @@ abstract class AbstractRepository
 
         return $result ?: null;
     }
+
+    abstract protected function getTableName(): string;
 
     abstract public function mapToObject(array $data);
 }
