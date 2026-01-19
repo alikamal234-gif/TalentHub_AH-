a0f029ca3027886c8096dfdd21a1fd59342674e0
Hamza Hajjaji <hh.hajjaji.hamza@gmail.com>
2026-01-19 22:01:43 +0100

    feat(container): ajouter un conteneur d'injection de dépendances
    - Implémentation d'un conteneur DI avec liaison de services
    - Ajout de méthodes pour enregistrer et récupérer des instances
    - Support de l'injection automatique via réflexion
    - Résolution des dépendances du constructeur
    - Gestion des paramètres nommés et des valeurs par défaut
    - Traitement des types primitifs et objets injectables
    - Mécanisme de mise en cache des instances créées
    - Gestion des erreurs lors de la résolution des dépendances

---
+++ core/Container/Container.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Core\Container;
+
+use ReflectionClass;
+use ReflectionException;
+use ReflectionMethod;
+use ReflectionNamedType;
+use ReflectionParameter;
+use RuntimeException;
+
+class Container
+{
+    private array $bindings = [];
+
+    public function bind(string $key, object $concrete): void
+    {
+        $this->bindings[$key] = $concrete;
+    }
+
+    public function has(string $key): bool
+    {
+        return isset($this->bindings[$key]);
+    }
+
+    public function get(string $key): object | string
+    {
+        if ($this->has($key)) {
+            return $this->bindings[$key];
+        }
+
+        $instance = $this->resolve($key);
+        $this->bindings[$key] = $instance;
+        return $instance;
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    public function call(array $callable, array $args = []): mixed
+    {
+        [$instance, $method] = $callable;
+        $reflection = new ReflectionMethod($instance, $method);
+
+        $dependencies = $this->resolveDependencies($reflection->getParameters(), $args);
+
+        return $reflection->invokeArgs($instance, $dependencies);
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    private function resolve(string $key) : object | string
+    {
+        if (class_exists($key)) {
+            $reflection = new ReflectionClass($key);
+            
+            if (!$reflection->isInstantiable()) {
+                throw new RuntimeException("La classe $key n'est pas instantiable");
+            }
+            
+            $constructor = $reflection->getConstructor();
+
+            // Si le constructeur n'existe pas, on retourne une nouvelle instance de la classe
+            if ($constructor === null) {
+                return new $key();
+            }
+
+            $dependencies = $this->resolveDependencies($constructor->getParameters());
+            
+            return $reflection->newInstanceArgs($dependencies);
+        }
+
+        throw new RuntimeException("Service '$key' not found or cannot be resolved.");
+    }
+
+    /**
+     * @param ReflectionParameter[] $parameters
+     * @param array $args
+     * @return array
+     */
+    private function resolveDependencies(array $parameters, array $args = []): array
+    {
+        $dependencies = [];
+        
+        foreach ($parameters as $parameter) {
+            $name = $parameter->getName();
+
+            // Si le paramètre est dans les arguments, on l'ajoute
+            if (array_key_exists($name, $args)) {
+                $dependencies[] = $args[$name];
+                continue;
+            }
+
+            $type = $parameter->getType();
+
+            if (!$type instanceof ReflectionNamedType || $type->isBuiltin()) {
+                if ($parameter->isDefaultValueAvailable()) {
+                    $dependencies[] = $parameter->getDefaultValue();
+                    continue;
+                }
+
+                if ($parameter->allowsNull()) {
+                    $dependencies[] = null;
+                    continue;
+                }
+
+                throw new RuntimeException("Impossible de résoudre le paramètre $name");
+            }
+
+            $dependencies[] = $this->get($type->getName());
+        }
+        
+        return $dependencies;
+    }
+}
