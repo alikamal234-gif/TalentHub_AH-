a6065afd8349650ceb448941f293410849c75acc
Hamza Hajjaji <hh.hajjaji.hamza@gmail.com>
2026-01-22 11:27:35 +0100

    feat(admin): ajouter la gestion des catégories dans l'interface d'administration
    - Création du contrôleur CategorieController avec les actions CRUD
    - Implémentation des méthodes pour lister, créer, modifier et supprimer les catégories
    - Ajout des vues Twig pour l'affichage de l'interface de gestion
    - Intégration des fonctionnalités de suppression douce (trashed)
    - Mise en place des routes pour l'administration des catégories
    - Création de la table HTML avec pagination et actions contextuelles

---
+++ src/Controller/Admin/CategorieController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Categorie;
+use App\Repository\CategorieRepository;
+use Core\Controller\AbstractController;
+use Core\Http\Request;
+use Core\Http\Response;
+
+class CategorieController extends AbstractController
+{
+    public function __construct(private CategorieRepository $categorieRepository)
+    {
+    }
+
+    public function index(): Response
+    {
+        return $this->render('admin/categorie/index.html.twig', ['categories' => $this->categorieRepository->findAll()]);
+    }
+
+    public function create(): Response
+    {
+        return $this->render('admin/categorie/create.html.twig');
+    }
+
+    public function store(Request $request)
+    {
+        $name = $request->input('name');
+
+        $categorie = new Categorie($name);
+        $this->categorieRepository->save($categorie);
+
+        $this->redirectToPath('/admin/categories');
+    }
+
+    public function edit(): Response
+    {
+        return $this->render('admin/categorie/edit.html.twig');
+    }
+
+    public function update(Request $request)
+    {
+    }
+
+    public function delete(Request $request)
+    {
+        $id = (int) $request->query('id');
+        $categorie = $this->categorieRepository->find($id);
+        if ($categorie) {
+            $this->categorieRepository->delete($categorie);
+        }
+
+        $this->redirectToPath('/admin/categories');
+    }
+
+    public function trashed(Request $request)
+    {
+        $id = (int) $request->query('id');
+        $categorie = $this->categorieRepository->find($id);
+        if ($categorie) {
+            $this->categorieRepository->trashed($categorie);
+        }
+
+        $this->redirectToPath('/admin/categories');
+    }
+}
