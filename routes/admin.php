ed9a36a9f2c3111e6e60b23c540515f107f2c151
Hamza Hajjaji <hh.hajjaji.hamza@gmail.com>
2026-01-22 11:26:05 +0100

    feat(admin): ajouter les routes pour la gestion des catégories
    - Ajout du contrôleur CategorieController
    - Création des routes pour l'affichage des catégories
    - Mise en place des routes pour la création de catégories
    - Ajout des routes pour la modification des catégories
    - Intégration des routes pour la suppression des catégories
    - Ajout de la route pour afficher les catégories supprimées

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,22 @@
 <?php
 
 use App\Controller\Admin\AdminController;
+use App\Controller\Admin\CategorieController;
 use Core\Router\Router;
 
 Router::get('/admin', [AdminController::class, 'admin']);
+
+// Router of categories
+Router::get('/admin/categories', [CategorieController::class, 'index']);
+
+// create
+Router::get('/admin/categories/create', [CategorieController::class, 'create']);
+Router::post('/admin/categories/store', [CategorieController::class, 'store']);
+
+// edit
+Router::get('/admin/categories/edit', [CategorieController::class, 'edit']);
+Router::post('/admin/categories/update', [CategorieController::class, 'update']);
+
+// delete
+Router::get('/admin/categories/delete', [CategorieController::class, 'delete']);
+Router::get('/admin/categories/trashed', [CategorieController::class, 'trashed']);
